0e1684983097bc25640edb17325b2010d90dcd4e
Ben Jones <ben@fogbutter.com>
2018-02-05 13:58:03 -0600

    added support for clusters
    - BREAKING CHANGE: drop support for "DBInstanceArn" return value. Use "Arn" instead.
    - BREAKING CHANGE: drop support for "DbiResourceId" return value. Use "ResourceId" instead"
    - added new return value "ReadEndpoint.Address" to support the related cluster value. For instances this will always be the same as "Endpoint.Address"

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 shell:
 	docker-compose run --rm python sh
 
+.PHONY: python
 python:
 	docker-compose run --rm python python
 
@@ -9,7 +10,7 @@ environment:
 
 clean:
 	docker-compose down
-	rm -rf .pytest_cache .coverage __pycache__
+	rm -rf .pytest_cache .coverage __pycache__ python/__pycache__
 
 .PHONY: tests
 tests:
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,7 @@
 import boto3
 from moto import mock_rds
 from index import lambda_handler
+from index import MSG_EMPTY_PROPS, MSG_AMBIGUOUS_PROPS, MSG_MISSING_PROPS
 
 
 def get_event():
@@ -13,17 +14,40 @@ def get_event():
     }
 
 
-def test_missing_params():
+def test_empty_params():
     event = get_event()
-    event['ResourceProperties'] = {"SomeGarbage": "DoNotWant"}
+    del event['ResourceProperties']
     response = lambda_handler(event)
     assert 'Status' in response
     assert response['Status'] == 'FAILED'
     assert response['StackId'] == event['StackId']
     assert response['LogicalResourceId'] == event['LogicalResourceId']
     assert response['RequestId'] == event['RequestId']
-    assert response['Reason'] == 'Required resource property ' \
-                                 'DBInstanceIdentifier is not set'
+    assert response['Reason'] == MSG_EMPTY_PROPS
+
+
+def test_ambiguous_params():
+    event = get_event()
+    event['ResourceProperties']['DBClusterIdentifier'] = 'my-cluster'
+    response = lambda_handler(event)
+    assert 'Status' in response
+    assert response['Status'] == 'FAILED'
+    assert response['StackId'] == event['StackId']
+    assert response['LogicalResourceId'] == event['LogicalResourceId']
+    assert response['RequestId'] == event['RequestId']
+    assert response['Reason'] == MSG_AMBIGUOUS_PROPS
+
+
+def test_missing_params():
+    event = get_event()
+    event['ResourceProperties'] = {'SomeGarbage': 'DoNotWant'}
+    response = lambda_handler(event)
+    assert 'Status' in response
+    assert response['Status'] == 'FAILED'
+    assert response['StackId'] == event['StackId']
+    assert response['LogicalResourceId'] == event['LogicalResourceId']
+    assert response['RequestId'] == event['RequestId']
+    assert response['Reason'] == MSG_MISSING_PROPS
 
 
 def test_delete():
@@ -37,7 +61,7 @@ def test_delete():
 
 
 @mock_rds
-def test_no_such_db():
+def test_no_such_instance():
     event = get_event()
     response = lambda_handler(event)
     assert 'Status' in response
@@ -51,7 +75,7 @@ def test_no_such_db():
 
 
 @mock_rds
-def test_success():
+def test_success_instance():
 
     instance_id = 'my-rds-instance'
     database_name = 'my-database'
@@ -87,8 +111,74 @@ def test_success():
     assert data['DBName'] == database_name
     assert data['Engine'] == engine
     assert data['EngineVersion'] == engine_version
-    # assert data['IAMDatabaseAuthenticationEnabled'] is True
-    assert 'DBInstanceArn' in data
+    assert 'Arn' in data
     assert 'Endpoint.Port' in data
     assert 'Endpoint.Address' in data
-    # assert 'DbiResourceId' in data
+    assert 'ReadEndpoint.Address' in data
+    assert data['ReadEndpoint.Address'] == data['Endpoint.Address']
+
+    # These are not supported by moto
+    # assert data['IAMDatabaseAuthenticationEnabled'] is True
+    # assert 'ResourceId' in data
+
+
+# moto has not implemented create_db_cluster so we'll skip these for now
+
+@mock_rds
+def skip_test_no_such_cluster():
+    event = get_event()
+    event["ResourceProperties"] = {"DBClusterIdentifier": "some-cluster"}
+    response = lambda_handler(event)
+    assert 'Status' in response
+    assert response['Status'] == 'FAILED'
+    assert response['StackId'] == event['StackId']
+    assert response['LogicalResourceId'] == event['LogicalResourceId']
+    assert response['RequestId'] == event['RequestId']
+    assert response['Reason'] == 'An error occurred (DBClusterNotFound) ' \
+                                 'when calling the DescribeDBClusters ' \
+                                 'operation: Cluster some-cluster not found.'
+
+
+@mock_rds
+def skip_test_success_cluster():
+
+    cluster_id = 'my-rds-cluster'
+    database_name = 'my-database'
+    engine = 'mysql'
+    engine_version = '1.2.3'
+    master_username = 'root'
+
+    # Create the database cluster
+    client = boto3.client('rds')
+    client.create_db_cluster(
+        DatabaseName=database_name,
+        DBClusterIdentifier=cluster_id,
+        Engine=engine,
+        EngineVersion=engine_version,
+        MasterUsername=master_username,
+        MasterUserPassword='override all security',
+        EnableIAMDatabaseAuthentication=True
+    )
+
+    event = get_event()
+    event['ResourceProperties']['DBClusterIdentifier'] = cluster_id
+    response = lambda_handler(event)
+    assert response['Status'] == 'SUCCESS'
+    assert response['StackId'] == event['StackId']
+    assert response['LogicalResourceId'] == event['LogicalResourceId']
+    assert response['PhysicalResourceId'] == cluster_id
+    assert response['RequestId'] == event['RequestId']
+
+    data = response['Data']
+    assert data['MasterUsername'] == master_username
+    assert data['DatabaseName'] == database_name
+    assert data['Engine'] == engine
+    assert data['EngineVersion'] == engine_version
+    assert 'Arn' in data
+    assert 'Endpoint.Port' in data
+    assert 'Endpoint.Address' in data
+    assert 'ReadEndpoint.Address' in data
+
+    # These are not supported by moto
+    # assert data['IAMDatabaseAuthenticationEnabled'] is True
+    # assert 'ResourceId' in data
